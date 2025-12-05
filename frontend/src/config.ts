c16c902411723c682a28503f6ebf565856eac7fe
dmoyarojas <leldavelol75@gmail.com>
2025-12-05 10:07:57 -0500

    last

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+// config.ts
+// Usar la variable de Vite (VITE_API_URL) y el objeto de entorno de Vite (import.meta.env)
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
